5a669c2b2cf7e1979085caa3772c81660b63af54
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-08-20 09:23:15 +0530

    Create Print_Geeks_For_Geeks.cpp

---
+++ C++ Basics/Print_Geeks_For_Geeks.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+ 
+
+// } Driver Code Ends
+//User function Template for C
+
+void print(){
+    
+    // prints Hello World.
+    // printf("Hello World\n");
+    
+    // prints GeeksforGeeks
+    printf("GeeksForGeeks\n");
+}
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+   
+    int t;
+    scanf("%d", &t);
+    
+    while(t--){
+        print();
+    }
+    
+    return 0;
+} 
+// } Driver Code Ends
