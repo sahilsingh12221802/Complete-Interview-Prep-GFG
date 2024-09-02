81f16a62865352d5dfb8774daf6b51875ec24997
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-09-02 21:02:37 +0530

    Create Print_Without_Space.cpp

---
+++ Input And Output In C++/Print_Without_Space.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+void utility(string a, string b){
+    //Write your code below that prints ab 
+
+    cout << a << b << endl;
+    //Write your code above that prints ab
+}
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+    int t ; cin>>t; 
+    while (t--) 
+    { 
+        string a, b;
+        cin>>a>>b;
+        utility(a, b);
+    } 
+    return 0;
+}
+// } Driver Code Ends
