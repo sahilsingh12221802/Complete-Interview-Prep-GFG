c8e98648c8b6d3ae5e4959bdfd18e1cea72b83a5
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-09-12 14:30:41 +0530

    Create Logical_Operators.cpp

---
+++ Operators/Logical_Operators.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+void utility(bool a, bool b){
+    //Do a AND b below
+    bool p = 
+    //Do a OR b below
+    bool q = 
+    //Do NOT a below
+    bool r = 
+
+    cout<< boolalpha << p << " " << q << " " << r << endl;
+}
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	while(t--) {
+        bool a, b;
+        cin>>a>>b;
+        utility(a, b);
+    }
+	
+}
+
+
+// } Driver Code Ends
