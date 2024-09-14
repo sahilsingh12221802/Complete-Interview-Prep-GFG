61529b80bb6e645fa960c5b0f982c34fab73a156
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-09-14 12:16:55 +0530

    Create Bitwise_Operators.cpp

---
+++ Operators/Bitwise_Operators.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+void utility(int a, int b, int c){
+    //Do a^a below
+    int d= a ^ a;
+    //Do c^b below
+    int e= c ^ b;
+    //Do a&b below
+    int f= a & b;
+    //Do c|(a^a) below
+    int g= c | (a^a);
+    //Do ~e below
+    e= ~e;
+
+     //The line below prints the output. Don't change it!
+    cout << d << " " << e << " " << f << " " << g <<endl;
+}
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	while(t--) {
+        int a, b, c;
+        cin>>a>>b>>c;
+        utility(a, b, c);
+    }
+	
+}
+// } Driver Code Ends
