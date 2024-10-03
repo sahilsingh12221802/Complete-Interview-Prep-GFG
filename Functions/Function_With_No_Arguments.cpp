2d07a705b756295aa9c09e2543efe46b413a189a
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-10-03 19:33:22 +0530

    Create Function_With_No_Arguments.cpp

---
+++ Functions/Function_With_No_Arguments.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//Write the helloFunction body below. It should have one statement
+//cout<<"Hello"<<endl;
+void helloFunction(){
+    cout<<"Hello"<<endl;
+}
+
+
+
+
+//Write the helloFunction body above. It should have one statement
+//cout<<"Hello"<<endl;
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	
+	while(t--){
+	    helloFunction();
+	}
+}
+// } Driver Code Ends
