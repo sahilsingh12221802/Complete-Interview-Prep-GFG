4146e4438398ef8535ea8dacbfe675516a691804
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-10-04 18:48:30 +0530

    Create Function_With_Arguments.cpp

---
+++ Functions/Function_With_Arguments.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//Write the complete argumentFunction below.
+//The function should take two arguments a and b
+//The function should have only one statement
+void argumentFunction(int a , int b){
+    cout << a + b << endl;
+}
+
+
+
+//Write the complete argumentFunction below.
+//The function should take two arguments a and b
+//The function should have only one statement
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	
+	while(t--){
+	    int a, b;
+	    cin>>a>>b;
+        argumentFunction(a, b);
+	}
+}
+// } Driver Code Ends
