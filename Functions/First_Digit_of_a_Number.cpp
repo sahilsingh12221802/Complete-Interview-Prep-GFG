5c4e6d5b86bd73c7b65c17adbe968b4028ae61d3
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-10-07 13:31:47 +0530

    Create First_Digit_of_a_Number.cpp

---
+++ Functions/First_Digit_of_a_Number.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Complete the function
+int firstDigit(int n){
+    // code here
+    while(n >= 10){
+        n = n / 10;
+    }
+    return n;
+    
+}
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	
+	while(t--){
+	    int n; cin>>n;
+            
+        int answer = firstDigit(n);
+        cout<<(answer)<<endl;
+	}
+}
+// } Driver Code Ends
