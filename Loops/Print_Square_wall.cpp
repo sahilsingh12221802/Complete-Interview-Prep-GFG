3fdbeb86de20bc2cf8c61e4ef5ee5de52e4a681b
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-10-16 22:43:34 +0530

    Create Print_Square_wall.cpp

---
+++ Loops/Print_Square_wall.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+void squareWall(int s){
+    
+    //Write your code here
+    for(int i = 1; i <= s; i++){
+        for(int j = 1; j <= s; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+}
+
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	
+	while(t--){
+	    int s; cin>>s;
+        squareWall(s);
+	}
+}
+// } Driver Code Ends
