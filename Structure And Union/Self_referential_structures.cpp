7d8be6cfcb44da514a6c6a6707d39b9c6dc1c259
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-12-01 11:18:51 +0530

    Create Self_referential_structures.cpp

---
+++ Structure And Union/Self_referential_structures.cpp
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+// } Driver Code Ends
+//User function Template for C++
+struct node{
+    int data;
+    node *next;
+};
+
+/*create linked list struct node*/
+// your code here
+
+
+//{ Driver Code Starts.
+#include <stdio.h>
+
+int main() {
+    // code
+
+    struct node ll;
+    printf("%d\n", 1);
+    printf("~");
+    return 0;
+}
+// } Driver Code Ends
