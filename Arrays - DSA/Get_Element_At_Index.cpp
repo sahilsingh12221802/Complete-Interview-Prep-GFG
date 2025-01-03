e2198c7e9b596939423aef04925eb3447c353731
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-01-03 13:49:31 +0530

    Create Get_Element_At_Index.cpp

---
+++ Arrays - DSA/Get_Element_At_Index.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//Complete this function
+int getByIndex(int arr[],int n,int i)
+{
+    //Your code here
+    if(i >= 0 && i < n){
+        return arr[i];
+    }
+    return - 1;
+}
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    
+	    int i;
+	    cin>>i;
+	    
+	    
+	    cout<<getByIndex(arr,n,i)<<endl;
+	    
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
