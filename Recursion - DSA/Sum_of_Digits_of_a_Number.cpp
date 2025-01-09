8f4635a15b1bd49d1857b1740ccb954373c00d6e
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-01-09 13:37:27 +0530

    Create Sum_of_Digits_of_a_Number.cpp

---
+++ Recursion - DSA/Sum_of_Digits_of_a_Number.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    // Complete this function
+    int sumOfDigits(int n)
+    {
+        //Your code here
+        if(n <= 9){
+            return n;
+        }
+        return sumOfDigits(n / 10) + n % 10;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() {
+	int T;
+	
+	//taking testcases
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    
+	    //taking input n
+	    cin>>n;
+	    
+	    //calling method sumOfDigits()
+	    Solution obj;
+	    cout<<obj.sumOfDigits(n)<<endl;
+	    
+	    
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
