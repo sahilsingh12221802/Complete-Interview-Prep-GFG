58516ca995ae678130826f19e833229822c34a58
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-02-11 14:19:30 +0530

    Create Sum_The_Nodes_of_Linked_List.cpp

---
+++ Linked List - DSA/Sum_The_Nodes_of_Linked_List.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+int sumOfElements(struct Node* head);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        cout << sumOfElements(head) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+*/
+
+int sumOfElements(Node *head)
+{
+   //Your code here
+   int sum = 0;
+   while(head){
+       sum += head->data;
+       head = head->next;
+   }
+   return sum;
+}
