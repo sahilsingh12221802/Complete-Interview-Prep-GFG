a556997bcb831b60e7231dfd76c74ac313edbb70
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-02-20 22:24:15 +0530

    Create Length_of_Circular_Linked_List.cpp

---
+++ Circular Linked List - DSA/Length_of_Circular_Linked_List.cpp
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+*/
+
+int getLength(Node * head)
+{
+    //Your code here
+    if(!head) return 0;
+    int count = 1;
+    Node* temp = head->next;
+    while(temp != head){
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+
+
+
+//{ Driver Code Starts.
+
+
+void makeCircular(Node * head)
+{
+    Node * temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+    }
+    temp->next=head;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail = NULL;
+	    int x;
+	    cin >> x;
+	    head = new Node(x);
+	    tail = head;
+	    for(int i=0;i<n-1;i++)
+	    {
+	        cin>>x;
+	        tail -> next = new Node(x);
+	        tail = tail -> next;
+	    }
+	    
+	    
+	 
+
+	    makeCircular(head);
+	    
+	    
+	    cout<<getLength(head);
+ 
+	    cout<<endl;
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
