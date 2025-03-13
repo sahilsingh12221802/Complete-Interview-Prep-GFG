3a9f0893077fd7a37bd40a67119613f0b3704ed7
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-03-13 13:16:32 +0530

    Create Find_Middle_of_Circular_Doubly_Linked_List.cpp

---
+++ Doubly Linked List/Find_Middle_of_Circular_Doubly_Linked_List.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+
+class Solution{
+    public:
+    int findMiddle(Node * head)
+    {
+       //Your code here
+       Node* slow = head, *fast = head;
+       while(fast->next != head && fast->next->next != head){
+           slow = slow->next;
+           fast = fast->next->next;
+       }
+       return slow->data;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+void makeCircular(Node * head)
+{
+    Node * temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+    }
+    
+    temp->next=head;
+    head->prev=temp;
+}
+
+
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail=NULL;
+        int x;
+	    cin>>x;
+	    head = newNode(x);
+	    tail = head;
+	    
+	    for(int i=0;i<n - 1;i++)
+	    {
+	        cin>>x;
+	        Node* temp=newNode(x);
+	        tail->next=temp;
+	        temp->prev= tail;
+	        tail = temp;
+	    }
+	    Solution ob;
+	    makeCircular(head);
+	    cout<<ob.findMiddle(head);
+ 
+	    cout<<endl;
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
