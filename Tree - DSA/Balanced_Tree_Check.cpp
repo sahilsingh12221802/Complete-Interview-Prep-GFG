4b325290b22623cda90194a5540703c55c585b52
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-03-28 18:16:52 +0530

    Create Balanced_Tree_Check.cpp

---
+++ Tree - DSA/Balanced_Tree_Check.cpp
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node structure
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Function to build a tree from a string representation
+Node* buildTree(string str) {
+    if (str.empty() || str[0] == 'N')
+        return nullptr;
+
+    vector<string> nodeValues;
+    istringstream iss(str);
+    for (string s; iss >> s;)
+        nodeValues.push_back(s);
+
+    Node* root = new Node(stoi(nodeValues[0]));
+    queue<Node*> nodeQueue;
+    nodeQueue.push(root);
+
+    size_t i = 1;
+    while (!nodeQueue.empty() && i < nodeValues.size()) {
+        Node* currentNode = nodeQueue.front();
+        nodeQueue.pop();
+
+        string leftValue = nodeValues[i++];
+        if (leftValue != "N") {
+            currentNode->left = new Node(stoi(leftValue));
+            nodeQueue.push(currentNode->left);
+        }
+
+        if (i >= nodeValues.size())
+            break;
+
+        string rightValue = nodeValues[i++];
+        if (rightValue != "N") {
+            currentNode->right = new Node(stoi(rightValue));
+            nodeQueue.push(currentNode->right);
+        }
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+/* A binary tree node structure
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    int checkHeight(Node* root) {
+       if(!root) return 0;
+       int lh = checkHeight(root->left);
+       if(lh == -1) return -1;
+       int rh = checkHeight(root->right);
+       if(rh == -1) return -1;
+       if(abs(lh - rh) > 1) return -1;
+       return max(lh, rh) + 1;
+    }
+    bool isBalanced(Node* root) {
+        // Code here
+        return checkHeight(root) != -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    cin.ignore(); // Ignore the newline after the number of test cases
+
+    while (testCases--) {
+        string treeInput;
+        getline(cin, treeInput);
+
+        Node* root = buildTree(treeInput);
+        Solution solution;
+        bool result = solution.isBalanced(root);
+
+        // Output "true" or "false"
+        cout << (result ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
