6ef21546d13b5e13e28c202323a95c7c89ff3d51
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-06-09 14:39:09 +0530

    update commit

--- Stack - Libraries/Get_Min_from_Stack.cpp
+++ Stack - Libraries/Get_Min_from_Stack.cpp
@@ -1,1 +1,28 @@
+class Solution {
+    stack<int> s, minS;
+  public:
+    Solution() {}
+
+    void push(int x) {
+        s.push(x);
+        if (minS.empty() || x <= minS.top()) minS.push(x);
+    }
+
+    void pop() {
+        if (!s.empty()) {
+            if (s.top() == minS.top()) minS.pop();
+            s.pop();
+        }
+    }
+
+    int peek() {
+        if (s.empty()) return -1;
+        return s.top();
+    }
+
+    int getMin() {
+        if (minS.empty()) return -1;
+        return minS.top();
+    }
+};
 
