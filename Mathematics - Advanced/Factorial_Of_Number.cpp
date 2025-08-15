fd38f8ce74e717e83e71cd74ef5c463e680f904a
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-08-15 22:21:57 +0530

    Create Factorial_Of_Number.cpp

---
+++ Mathematics - Advanced/Factorial_Of_Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int factorial(int n) {
+        int result = 1;
+        for(int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+};
