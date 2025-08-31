c634e7450f11e103210f4542a7d99bd4f158f90a
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-08-31 16:55:53 +0530

    Create Gray_to_Binary_Conversion.cpp

---
+++ Bit Magic - Advanced/Gray_to_Binary_Conversion.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int grayToBinary(int n) {
+        int res = n;
+        while(n > 0) {
+            n >>= 1;
+            res ^= n;
+        }
+        return res;
+    }
+};
