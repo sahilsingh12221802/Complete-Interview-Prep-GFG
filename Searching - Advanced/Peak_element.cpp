482d95252eaa111ed5537fdf04265f89c5f05868
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-10-03 19:40:38 +0530

    Create Peak_element.cpp

---
+++ Searching - Advanced/Peak_element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        int n = arr.size();
+        int l = 0, r = n - 1;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(arr[mid] < arr[mid + 1]) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+};
