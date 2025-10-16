a2195d207dacf71f90e9781cdd5e366745fd05b5
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-10-16 19:38:48 +0530

    Create Count_Inversions.cpp

---
+++ Sorting - Advanced/Count_Inversions.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    long long merge(vector<int>& arr, vector<int>& temp, int left, int mid, int right) {
+        int i = left, j = mid, k = left;
+        long long inv_count = 0;
+        while (i <= mid - 1 && j <= right) {
+            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
+            else {
+                temp[k++] = arr[j++];
+                inv_count += (mid - i);
+            }
+        }
+        while (i <= mid - 1) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+        for (i = left; i <= right; i++) arr[i] = temp[i];
+        return inv_count;
+    }
+    
+    long long mergeSort(vector<int>& arr, vector<int>& temp, int left, int right) {
+        long long inv_count = 0;
+        if (right > left) {
+            int mid = (right + left) / 2;
+            inv_count += mergeSort(arr, temp, left, mid);
+            inv_count += mergeSort(arr, temp, mid + 1, right);
+            inv_count += merge(arr, temp, left, mid + 1, right);
+        }
+        return inv_count;
+    }
+    
+    int inversionCount(vector<int> &arr) {
+        vector<int> temp(arr.size());
+        return mergeSort(arr, temp, 0, arr.size() - 1);
+    }
+};
