fcf953baca78fc793c628ab185d00f11e1107eae
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-10-19 10:09:51 +0530

    Create Count_the_number_of_possible_triangles.cpp

---
+++ Sorting - Advanced/Count_the_number_of_possible_triangles.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int countTriangles(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size(), count = 0;
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+};
