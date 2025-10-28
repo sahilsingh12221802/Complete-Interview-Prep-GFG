821baa447a1fcf4f2dc54740d772f627e44cf5b8
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-10-28 15:28:19 +0530

    Create Minimum_Platforms.cpp

---
+++ Sorting - Advanced/Minimum_Platforms.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // Your code here
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
+        int platforms = 0, maxPlatforms = 0, i = 0, j = 0, n = arr.size();
+        while (i < n) {
+            if (arr[i] <= dep[j]) {
+                platforms++;
+                maxPlatforms = max(maxPlatforms, platforms);
+                i++;
+            } else {
+                platforms--;
+                j++;
+            }
+        }
+        return maxPlatforms;
+    }
+};
+
+
+
+
+
+
+
+
