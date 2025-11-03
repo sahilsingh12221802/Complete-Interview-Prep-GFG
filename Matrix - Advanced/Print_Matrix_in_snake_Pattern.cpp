b2f7fa62d3964ca5222eb177f5a30b7adc04bd9a
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-11-03 19:19:26 +0530

    Create Print_Matrix_in_snake_Pattern.cpp

---
+++ Matrix - Advanced/Print_Matrix_in_snake_Pattern.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<int> snakePattern(vector<vector<int>> matrix) {
+        int n = matrix.size();
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++)
+                    ans.push_back(matrix[i][j]);
+            } else {
+                for (int j = n - 1; j >= 0; j--)
+                    ans.push_back(matrix[i][j]);
+            }
+        }
+        return ans;
+    }
+};
