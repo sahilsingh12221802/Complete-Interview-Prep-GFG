ab553ed7fb0ff25e1d3d4a10e0ced16ac88beca7
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-11-11 16:08:49 +0530

    Create Spirally_traversing_a_matrix.cpp

---
+++ Matrix - Advanced/Spirally_traversing_a_matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
+        vector<int> res;
+        int n = mat.size(), m = mat[0].size();
+        int top = 0, bottom = n - 1, left = 0, right = m - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) res.push_back(mat[top][i]);
+            top++;
+            for (int i = top; i <= bottom; i++) res.push_back(mat[i][right]);
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) res.push_back(mat[bottom][i]);
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) res.push_back(mat[i][left]);
+                left++;
+            }
+        }
+        return res;
+    }
+};
